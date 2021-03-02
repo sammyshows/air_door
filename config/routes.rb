c02354db05e4bee3b5cf135762b2299e5a424bc7
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-02 17:22:13 +1100

    Implemented form into the bookings page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   resources :portals, only: [:index, :show, :new, :create, :edit] do
     resources :bookings, only: [:new, :create]
-    resources :dashboards, only: [:index, :update]
   end
+
+  get "dashboard" , to: "dashboards#show"
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
