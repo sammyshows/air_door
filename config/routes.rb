b5f69c55df95c303380ec48a870e6939c2820452
Thembi Baleni <thembi.baleni@gmail.com>
2021-03-02 17:52:10 +1100

    added method for dashboard and updated route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   resources :portals, only: [:index, :show, :new, :create, :edit] do
     resources :bookings, only: [:new, :create]
-    resources :dashboards, only: [:index, :update]
   end
+  resource :dashboard, only: [:show]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
