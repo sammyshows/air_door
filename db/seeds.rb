09c3808fe4509acd1778a6ef8bcf98c356aaf69e
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-03 14:57:39 +1100

    Added two users to seeds file and divided portals among them

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # User.create(email: "user@user.com", password: "password")
 # User.last.create(name: "Totally Lit", description: "yeah its sweet", price: 3500)
+Booking.delete_all
+Portal.delete_all
+User.delete_all
+
+User.create(email: "userone@user.com", password: "password")
+User.create(email: "usertwo@user.com", password: "password")
 
 pyramids_of_giza = { name: "Pyramids of Giza", description: "A few triangles", price: 1000 }
 great_barrier_reef = { name: "Great Barrier Reef", description: "Beautiful coral", price: 1000 }
@@ -14,7 +20,10 @@ stonehenge = { name: "Stonehenge", description: "A few rocks", price: 1000 }
 great_wall_of_china = { name: "Great Wall of China", description: "A wall", price: 1000 }
 mt_everest = { name: "Mount Everest", description: "A hill", price: 1000 }
 
+[pyramids_of_giza, great_barrier_reef].each do |attributes|
+  User.first.portals.create(attributes)
+end
 
-[pyramids_of_giza, great_barrier_reef, stonehenge, great_wall_of_china, mt_everest].each do |attributes|
+[stonehenge, great_wall_of_china, mt_everest].each do |attributes|
   User.last.portals.create(attributes)
 end
