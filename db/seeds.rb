f954d62a30dc63eb059e335902277954b246124a
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-05 14:37:39 +1100

    Added two taj mahal seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,3 +72,10 @@ while i < url.length
   Portal.find(i+1).cover_photo.attach(io: file, filename: "#{Portal.find(i+1).name.downcase.gsub(' ', '_')}.jpg", content_type: 'image/jpg')
   i += 1
 end
+
+file = URI.open("https://images.unsplash.com/photo-1544622428-56b8d9eed7db?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1500&q=80")
+Portal.find(15).photos.attach(io: file, filename: "tajmahal1.jpg", content_type: 'image/jpg')
+
+file = URI.open("https://images.unsplash.com/photo-1575489181784-a99aa58e4bb5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2791&q=80")
+Portal.find(15).photos.attach(io: file, filename: "tajmahal2.jpg", content_type: 'image/jpg')
+
